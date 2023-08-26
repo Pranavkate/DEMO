748f8aabfd92af42db6fd514045a16b988faac4c
Pranavkate <katepranav976@gmail.com>
2023-08-26 15:27:40 +0530

    Game

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const winner = calculateWinner(squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board">
+        {squares.map((square, i) => (
+          <Square key={i} value={square} onClick={() => handleClick(i)} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (const combo of winningCombos) {
+    const [a, b, c] = combo;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic Tac Toe</h1>
+        <Board />
+      </header>
+    </div>
+  );
+}
+
+export default App;
